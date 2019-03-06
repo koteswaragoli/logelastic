1664e5cffcb93e164ddb0cd18163c911e659f339
Rubin Simons <rubin@raaf.tech>
2019-03-06 16:00:38 +0100

    Added System.out.println lines to the Test so you can see async working by looking at line output of the test.

--- src/test/java/tech/raaf/logelastic/log4j/appender/ElasticAppenderTest.java
+++ src/test/java/tech/raaf/logelastic/log4j/appender/ElasticAppenderTest.java
@@ -10,9 +10,13 @@ public class ElasticAppenderTest {
     @Test
     public void testSomeLoggingMessages() {
         Logger logger = LogManager.getLogger(ElasticAppenderTest.class.getName());
+        System.out.println("About to print first log message..");
         logger.debug("this is a message from me, world!");
+        System.out.println("About to print second log message..");
         logger.warn("But what is this");
+        System.out.println("About to print third log message..");
         logger.error("Oh no, more lemmings!");
+        System.out.println("About to print last log message..");
         logger.info("Verily I say unto thee, end thyself, logger!");
     }
 }
